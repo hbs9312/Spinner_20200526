e8a4f66ee2c5ec3f5e45f23fef8d0ea4d4fc0ec1
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2020-05-26 16:19:38 +0900

    스피너를 리스트뷰 처럼 작성  - 실 데이터 뿌려주기만 남음

--- app/src/main/java/kr/co/tjoeun/spinner_20200526/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/spinner_20200526/MainActivity.java
@@ -5,11 +5,19 @@ import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import kr.co.tjoeun.spinner_20200526.adapters.OccupationSpinnerAdapter;
 import kr.co.tjoeun.spinner_20200526.databinding.ActivityMainBinding;
+import kr.co.tjoeun.spinner_20200526.datas.Occupation;
 
 public class MainActivity extends BaseActivity {
 
     ActivityMainBinding binding;
+    List<Occupation> occupations = new ArrayList<>();
+
+    OccupationSpinnerAdapter osa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,15 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+        addOccupations();
+        osa = new OccupationSpinnerAdapter(mContext, R.layout.occupation_spinner_list_item, occupations);
+        binding.occSpinner.setAdapter(osa);
+    }
 
+    void addOccupations() {
+        occupations.add(new Occupation("안드로이드", R.drawable.andorid_icon));
+        occupations.add(new Occupation("iOS", R.drawable.ios_icon));
+        occupations.add(new Occupation("웹프론트", R.drawable.web_icon));
+        occupations.add(new Occupation("서버", R.drawable.server_icon));
     }
 }
